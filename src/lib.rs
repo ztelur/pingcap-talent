4278721fc956f82c33239abb56036717717b9c85
Randy <ztelur@gmail.com>
2021-08-30 18:50:55 +0800

    update

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+
+
+
+pub struct KvStore {}
+
+
+impl KvStore {
+    pub fn new() -> KvStore {
+        panic!()
+    }
+
+    pub fn set(&mut self, key: String, value: String) {
+        panic!()
+    }
+
+    pub fn get(&self, key: String) -> Option<String> {
+        panic!()
+    }
+
+    pub fn remove(&mut self, key: String) {
+        panic!()
+    }
+}
